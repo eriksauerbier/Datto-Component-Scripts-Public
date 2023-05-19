13a13b9953830e34d1ec90d9346aa06860d4ea43
Erik Sauerbier <erik.sauerbier@mailbox.org>
2023-05-19 07:11:03 +0200

    v.1.1.1
    - Skript kopiert nun Dateien in Unterordnern richtig

--- ComponentScript-Update-ADMX-Files.ps1
+++ ComponentScript-Update-ADMX-Files.ps1
@@ -1,5 +1,5 @@
 # Datto Skript zum Aktualisieren von ADMX-Files inkl. Fehlerausgabe
-# Stannek GmbH - v.1.0.4 - 13.01.2023 - E.Sauerbier
+# Stannek GmbH - v.1.1.1 - 19.05.2023 - E.Sauerbier
 
 # Parameter
 $UtilPath = "C:\Util"
@@ -12,15 +12,18 @@ $Zip = Get-ChildItem -Filter $env:ZIPName
 # Archiv entpacken 
 Expand-Archive -Path $(".\"+$Zip) -DestinationPath $UtilPath -Force
 
-# ADMX-Files kopieren
-Move-Item -Path $($UtilPath+"\"+$Zip.Name.Replace('.zip','')+"\*.*") -Destination $PDPath -Force -ErrorVariable MoveError
+# Zu kopierende Dateien auslesen
+$files = Get-ChildItem -Path $($UtilPath+"\"+$Zip.Name.Replace('.zip','')) -Recurse -File
 
-# Pruefen ob es einen CentralPolicy-Store gibt und ADMX-Files entsprechend kopieren 
-if (Test-Path $CPDPath) {Move-Item -Path $($UtilPath+"\"+$Zip.Name.Replace('.zip','')+"\*.*") -Destination $CPDPath -Force -ErrorVariable MoveError}
+# ADMX-Files in Standardpfad kopieren
+foreach ($file in $files) {Copy-Item -Path $file.FullName -Destination $(Join-Path -Path $PDPath -ChildPath $file.Directory.Name) -Force -Verbose -ErrorVariable MoveError}
+
+# Pruefen ob es einen CentralPolicy-Store gibt und auch ADMX-Files entsprechend kopieren
+if (Test-Path $CPDPath) {foreach ($file in $files) {Move-Item -Path $file.FullName -Destination $(Join-Path -Path $CPDPath -ChildPath $file.Directory.Name) -Force -Verbose -ErrorVariable MoveError}}
 
 # Remove expanded Archiv
 Remove-Item -Path $($UtilPath+"\"+$Zip.Name.Replace('.zip','')) -Recurse -Force
 
 # inform the user
-if ("" -eq $MoveError) {write-host "Script has been updated"}
+if ("" -eq $MoveError) {write-host "ADMX-Files has been updated"}
 Else {Throw "There is a problem while moving: $MoveError"}
