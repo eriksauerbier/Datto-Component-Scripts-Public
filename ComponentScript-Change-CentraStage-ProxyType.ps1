40d78e1fe90e96da3eac42623f1ee97f2c332e71
eriksauerbier <erik.sauerbier@mailbox.org>
2023-12-04 15:28:18 +0100

    v.1.1.1
    - Skript um Proxy-Port und Proxy-Adresse Erweitert

--- ComponentScript-Change-CentraStage-ProxyType.ps1
+++ ComponentScript-Change-CentraStage-ProxyType.ps1
@@ -1,5 +1,5 @@
 # Datto Skript zum anpassen des ProxyTypes vom RMM-Agent
-# Stannek GmbH - v.1.0 - 28.09.2022 - E.Sauerbier
+# Stannek GmbH - v.1.1.1 - 04.12.2023 - E.Sauerbier
 
 #Parameter
 $PathRMMConfig = Join-Path -Path $env:SystemDrive -ChildPath "Program Files (x86)\CentraStage\CagService.exe.config"
@@ -8,7 +8,13 @@ $PathRMMConfig = Join-Path -Path $env:SystemDrive -ChildPath "Program Files (x86
 [XML]$FileXML = Get-Content -Path $PathRMMConfig
 
 # ProxyType anpassen
-$FileXML.configuration.applicationSettings.'CentraStage.Cag.Core.AppSettings'.SelectSingleNode("//setting[@name='ProxyType']").Value = "0"
+$FileXML.configuration.applicationSettings.'CentraStage.Cag.Core.AppSettings'.SelectSingleNode("//setting[@name='ProxyType']").Value = "3"
+
+# ProxyIP anpassen
+$FileXML.configuration.applicationSettings.'CentraStage.Cag.Core.AppSettings'.SelectSingleNode("//setting[@name='ProxyIp']").Value = "win-proxy.services.datevnet.de"
+
+# Proxy-Port anpassen
+$FileXML.configuration.applicationSettings.'CentraStage.Cag.Core.AppSettings'.SelectSingleNode("//setting[@name='ProxyPort']").Value = "8880"
 
 # angepasstes XML-File zurückschreiben
 $FileXML.Save("$PathRMMConfig")
