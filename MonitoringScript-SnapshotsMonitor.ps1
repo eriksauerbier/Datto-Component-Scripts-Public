2624b5c376c390efe6942ba958606cabeae490cc
eriksauerbier <erik.sauerbier@mailbox.org>
2023-12-19 12:35:13 +0100

    v.1.1
    - fehlende Funktion implementiert
    - Fehlerausgabe korrigiert

--- MonitoringScript-SnapshotsMonitor.ps1
+++ MonitoringScript-SnapshotsMonitor.ps1
@@ -1,13 +1,25 @@
 # Datto Monitoring-Skript zum Abfragen von vorhandenen Snaptshot auf einem Hyper-V Host
-# Stannek GmbH - v.1.0 - 04.12.2023 - E.Sauerbier
+# Stannek GmbH - v.1.1 - 19.12.2023 - E.Sauerbier
 
 # Parameter
 $DaysToWarn = "1"
+
+# Funktionen laden
+function Write-RMMDiag ($messages) {
+    write-host '<-Start Diagnostic->'
+    foreach ($Message in $Messages) { $Message }
+    write-host '<-End Diagnostic->'
+    } 
+function Write-RMMAlert ($message) {
+    write-host '<-Start Result->'
+    write-host "R=$message"
+    write-host '<-End Result->'
+} 
 
 # Snapshot auslesen
 $SnapShots = Get-VM | Get-VMSnapshot | Select-Object VMName,Name,SnapshotType,CreationTime,ComputerName
 
 # Checken ob Snapshots vorhanden sind und ob diese aelter als x-Tage sind
-If ($SnapShots.VMName.Count -eq "0") {Write-Host "Keine Snapshots vorhanden";Exit 1}
-Elseif ($SnapShots.CreationTime -le $(Get-Date).AddDays(-$DaysToWarn)) {Write-RRMAlert "Folgende VMs haben Snapshots $SnapShots.VMName die aelter als $DaysToWarn Tage sind"}
-Else {Write-Host "Snapshots von $($SnapShots.VMName) vorhanden aber noch im Zeitlichen Rahmen";Exit 1}
+If ($SnapShots.VMName.Count -eq "0") {Write-RMMAlert "Keine Snapshots vorhanden";Exit 0}
+Elseif ($SnapShots.CreationTime -le $(Get-Date).AddDays(-$DaysToWarn)) {Write-RMMAlert "Folgende VMs haben Snapshots $($SnapShots.VMName) die aelter als $DaysToWarn Tage sind";Exit 1}
+Else {Write-RMMAlert "Snapshots von $($SnapShots.VMName) vorhanden aber noch im Zeitlichen Rahmen";Exit 0}
